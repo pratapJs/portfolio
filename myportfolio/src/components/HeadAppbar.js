f258f0b0474d2485776f3d1f35c8e6b05b5cace0
savip <pratapadhikari81@gmail.com>
2020-06-30 13:59:43 +0930

    fully functional Navbar(device size compatible) done before refactoring

--- myportfolio/src/components/HeadAppbar.js
+++ myportfolio/src/components/HeadAppbar.js
@@ -10,9 +10,15 @@ import Menu from "@material-ui/core/Menu"
 import MenuItem from "@material-ui/core/MenuItem"
 import useMediaQuery from "@material-ui/core/useMediaQuery"
 import { useTheme } from "@material-ui/core/styles"
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer"
+import MenuIcon from "@material-ui/icons/Menu"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
 
 import { makeStyles } from "@material-ui/core/styles"
 import logo from "../assets/logo.svg"
+import { IconButton } from "@material-ui/core"
 
 function ElevationScroll(props) {
   const { children } = props
@@ -31,10 +37,10 @@ const useStyles = makeStyles(theme => ({
     ...theme.mixins.toolbar,
     marginBottom: "1.60em",
     [theme.breakpoints.down("md")]: {
-      marginBottom: "0.8em"
+      marginBottom: "0.9em"
     },
     [theme.breakpoints.down("xs")]: {
-      marginBottom: ".40em"
+      marginBottom: ".80em"
     }
   },
   logo: {
@@ -80,24 +86,52 @@ const useStyles = makeStyles(theme => ({
     marginLeft: "50px",
     marginRight: "25px",
     color: "white"
+  },
+  drawerIconContainer: {
+    marginLeft: "auto",
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  },
+  drawerIcon: {
+    height: "50px",
+    width: "50px"
+  },
+  drawer: {
+    backgroundColor: theme.palette.common.green
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    fontFamily: "Roboto",
+    color: "white",
+    opacity: 0.7
+  },
+  drawerItemHireMe: {
+    backgroundColor: theme.palette.common.orange,
+    borderRadius: "10px"
+  },
+  drawerItemSelected:{
+    opacity:1
   }
 }))
 
 export default function HeadAppbar(props) {
   const classes = useStyles()
   const theme = useTheme()
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent)
   const matches = useMediaQuery(theme.breakpoints.down("md"))
   const [value, setValue] = useState(0)
   const [anchorEl, setAnchorEl] = useState(null)
   const [selectedIndex, setSelectedIndex] = useState(0)
+  const [openDrawer, setOpenDrawer] = useState(false)
 
   const handleMenuItemClick = (e, i) => {
     setSelectedIndex(i)
     setAnchorEl(null)
   }
 
-  const handleChange = (e, value) => {
-    setValue(value)
+  const handleChange = (e, newValue) => {
+    setValue(newValue)
   }
   const handleClick = e => {
     setAnchorEl(e.currentTarget)
@@ -192,6 +226,108 @@ export default function HeadAppbar(props) {
       </Menu>
     </>
   )
+  const drawer = (
+    <React.Fragment>
+      <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer} onClose={() => setOpenDrawer(false)} onOpen={() => setOpenDrawer(true)} classes={{ paper: classes.drawer }}>
+        <List disablePadding>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false)
+              setValue(0)
+            }}
+            divider
+            button
+            component={Link}
+            to="/"
+            selected={value === 0}
+          >
+            <ListItemText className={value===0 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem} disableTypography>
+              Home
+            </ListItemText>
+          </ListItem>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false)
+              setValue(1)
+            }}
+            divider
+            button
+            component={Link}
+            to="/about"
+            selected={value === 1}
+          >
+            <ListItemText className={value===1 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem} disableTypography>
+              About Me
+            </ListItemText>
+          </ListItem>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false)
+              setValue(2)
+            }}
+            divider
+            button
+            component={Link}
+            to="/projects"
+            selected={value === 2}
+          >
+            <ListItemText className={value===2 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem} disableTypography>
+              Projects
+            </ListItemText>
+          </ListItem>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false)
+              setValue(3)
+            }}
+            divider
+            button
+            component={Link}
+            to="/contact"
+            selected={value === 3}
+          >
+            <ListItemText className={value===3 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem} disableTypography>
+              Contact
+            </ListItemText>
+          </ListItem>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false)
+              setValue(4)
+            }}
+            divider
+            button
+            component={Link}
+            to="/services"
+            selected={value === 4}
+          >
+            <ListItemText className={value===4 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem} disableTypography>
+              Services
+            </ListItemText>
+          </ListItem>
+          <ListItem
+            className={classes.drawerItemHireMe}
+            onClick={() => {
+              setOpenDrawer(false)
+              setValue(5)
+            }}
+            divider
+            button
+            component={Link}
+            to="/hireme"
+            selected={value === 5}
+          >
+            <ListItemText className={classes.drawerItem} disableTypography>
+              Hire Me
+            </ListItemText>
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+      <IconButton className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
+        <MenuIcon className={classes.drawerIcon} />
+      </IconButton>
+    </React.Fragment>
+  )
 
   return (
     <>
@@ -202,7 +338,7 @@ export default function HeadAppbar(props) {
               <img className={classes.logo} alt="Portfolio logo" src={logo} />
             </Button>
 
-            {matches ? null : tabs}
+            {matches ? drawer : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
