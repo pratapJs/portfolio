9841640dc40a42b3585fefe65f7869fe565a65fe
savip <pratapadhikari81@gmail.com>
2020-07-01 19:42:04 +0930

    Footer done (responsive design)

--- myportfolio/src/components/App.js
+++ myportfolio/src/components/App.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
 import HeadAppbar from "./HeadAppbar"
+import Footer from "./Footer"
 import { ThemeProvider } from "@material-ui/styles"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import theme from "../ui/Theme"
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [value, setValue] = useState(0)
+
   return (
-    <div>
+  
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <HeadAppbar />
+          <HeadAppbar value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
           <Switch>
             <Route exact path="/" component={() => <div> Home </div>} />
             <Route exact path="/about" component={() => <div> About Me</div>} />
@@ -20,9 +24,11 @@ function App() {
             <Route exact path="/websitedevelopment" component={() => <div> Website Development </div>} />
             <Route exact path="/hireme" component={() => <div> Hire Me </div>} />
           </Switch>
+          <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
         </BrowserRouter>
+      
       </ThemeProvider>
-    </div>
+
   )
 }
 
